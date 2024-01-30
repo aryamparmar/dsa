69f6c694e63df6db6c8d18eb850e62ccc8edda0f
Aryam <aryamsingh7676@gmail.com>
2024-01-30 17:44:52 +0530

    new repo

---
+++ aryam.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mod (long long)1000000007
+
+#define ll long long int
+
+#define ld long double
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+return 0; 
+}
